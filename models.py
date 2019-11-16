8448937012a5453e4c33c9b7e917757241215f5f
Ethan Chi <ethanchi@stanford.edu>
2019-11-16 01:40:46 -0800

    added destructor

--- models.py
+++ models.py
@@ -1,15 +1,80 @@
+# https://github.com/dmlc/dgl/blob/master/examples/pytorch/dgmg/model.py
+
+
+
 import torch.nn as nn
 import dgl
 from util import *
 class GraphDestructor(nn.Module):
     # returns the inverse order of nodes and edges by destruction order
-    NotImplemented
+    def __init__(self, node_act_dim=NODE_ACT_DIM, node_hidden_size=NODE_HIDDEN_SIZE):
+        super(GraphDestructor, self).__init__():
+        self.graph_embed = GraphEmbed(node_hidden_size)
+        # rounds, node_dim, node_act_dim, edge_dim
+        self.graph_prop = GraphProp(num_prop_rounds, node_hidden_size, node_act_dim, g.number_of_edges())
+        self.choose_victim_agent = ChooseVictimAgent(self.graph_prop, node_hidden_size)
+    def forward_inference(self):
+        while self.g.number_of_nodes() > 1:
+            self.choose_victim_agent()
+        return self.g.nodes[0].data['hv']
+    def forward_train(self, order):
+        self.prepare_for_train()
+        i = 0
+        while self.g.number_of_nodes() > 1:
+            self.choose_victim_agent(a=actions[i])
+            i += 1
+        return self.get_log_prob()
+    def get_log_prob(self):
+        return torch.cat(self.choose_victim_agent.log_prob).sum()
+    def forward(self, victims=None):
+        # The graph we will work on
+        self.g = dgl.DGLGraph()
 
+        # If there are some features for nodes and edges,
+        # zero tensors will be set for those of new nodes and edges.
+        self.g.set_n_initializer(dgl.frame.zero_initializer)
+        self.g.set_e_initializer(dgl.frame.zero_initializer)
+
+        if self.training:
+            return self.forward_train(actions)
+        else:
+            return self.forward_inference()
+
+class ChooseVictimAgent(nn.Module):
+    def __init__(self, graph_prop_func, node_hidden_size):
+        super(ChooseVictimAgent, self).__init__()
+
+        self.graph_op = {'prop': graph_prop_func}
+        self.choose_death = nn.Linear(2 * node_hidden_size, 1)
+
+    def _initialize_edge_repr(self, g, src_list, dest_list):
+        # For untyped edges, we only add 1 to indicate its existence.
+        # For multiple edge types, we can use a one hot representation
+        # or an embedding module.
+        edge_repr = torch.ones(len(src_list), 1)
+        g.edges[src_list, dest_list].data['he'] = edge_repr
+
+    def prepare_training(self):
+        self.log_prob = []
+
+    def forward(self, g, trueVictim):
+        node_embeddings = g.ndata
+        print("node embedding shape", node_embeddings.shape)
+        death_probs = self.choose_death(node_embeddings)
+        death_probs = F.softmax(death_probs, dim=1)
+
+        if not self.training:
+            victim = Categorical(death_probs).sample().item()
+            g.remove_nodes(victim)
+
+        if self.training:
+            if dests_probs.nelement() > 1:
+                self.log_prob.append(F.log_softmax(death_probs, dim=1)[:, trueVictim:trueVictim+1])
 '''
 Implementation adapted from https://docs.dgl.ai/en/latest/tutorials/models/3_generative_model/5_dgmg.html
 '''
 class GraphConstructor(nn.Module):
-    # TODO: 
+    # TODO:
     def __init__(self, z_dim, dims):
         super(GraphConstructor, self).__init__()
 
@@ -24,7 +89,7 @@ class GraphConstructor(nn.Module):
                 num_trials += 1
                 to_add_edge = self.add_edge_or_not()
             stop = self.add_node_and_update()
-        
+
         return self.g
 
     def add_node_and_update(self, a=None):
@@ -52,7 +117,7 @@ class GraphConstructor(nn.Module):
             while to_add_edge:
                 self.choose_dest_and_update(a=actions[self.action_step])
             stop = self.add_node_and_update(a=actions[self.action_step])
-        
+
         return self.get_log_prob()
 
     def forward_inference(self):
@@ -97,7 +162,7 @@ class GraphProp(nn.Module):
         '''
         super(GraphProp, self).__init__()
         self.node_act_dim = node_act_dim
-        
+
         # Each propogation step has same dims but different params
         self.message_funcs = [] # Functions transformating a cocatenation of hu, hv, xuv to a vector
         self.reduce_funcs = [] # Sums incoming messages to be activation
@@ -106,7 +171,7 @@ class GraphProp(nn.Module):
             self.message_funcs.append(nn.Linear(2 * node_dim + edge_dim, node_act_dim))
             self.reduce_funcs.append(partial(self.dgmg_reduce, round=t))
             self.node_update_funcs.append(nn.GRUCell(self.node_act_dim, node_dim))
-        
+
         # Originally ModuleLists
 
     def dgmg_msg(self, edges):
@@ -121,7 +186,7 @@ class GraphProp(nn.Module):
         node_activation = (self.message_funcs[round](message)).sum(1)
 
         return {'a':node_activation}
-    
+
     def forward(self, g):
         if g.number_of_edges() > 0:
             for t in range(self.rounds):
@@ -159,10 +224,10 @@ class AddNode(nn.Module):
         if acction == 1: # not stop
             g.add_nodes(1)
             self.initialize_node(g, graph_embed)
-        
+
         log_prob = bernoulli_action_log_prob(logit, action)
         self.log_prob.append(log_prob)
-        
+
         return action
 
 class AddEdge(nn.Module):
@@ -171,7 +236,7 @@ class AddEdge(nn.Module):
 
         self.graph_embed_func = graph_embed_func
         self.add_edge = nn.Linear(graph_embed_func.graph_dim + node_dim, num_edge_types)
-        
+
         self.log_prob = []
 
         # Static edge types
@@ -187,8 +252,8 @@ class AddEdge(nn.Module):
         # Incomplete
 
 
-        
-        
+
+
 
 
 
@@ -205,4 +270,4 @@ if __name__ == "__main__":
     #   edges[src, dest].data similarly, also as .edges[[src list], [dest list]].data
     #   g.edata is not intuitive sorted, so be careful
     #   Careful when setting schemes
-    #   
+    #
